dbd7f8e0fd1fa73db7ed46e607295099dddb0531
namaste-code <shitanshu.19p@gmail.com>
2019-05-08 17:19:10 +0530

    Making queries and binding data in the BookList

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
 
-export default class BookList extends Component {
+const getBooksQuery = gql`
+    {
+        books {
+            name
+            id
+        }
+    }
+`
+
+class BookList extends Component {
   render() {
-    return (
-      <div>
-        <ul id="book-list">
-            <li>Book name</li>
-        </ul>
-      </div>
-    )
+        console.log(this.props);
+        return (
+        <div>
+            <ul id="book-list">
+                <li>Book name</li>
+            </ul>
+        </div>
+        )
   }
 }
+
+export default graphql(getBooksQuery)(BookList);
