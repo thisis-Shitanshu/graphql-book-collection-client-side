5a38f0aeadc666b29b0606f612f20a0e0043b514
namaste-code <shitanshu.19p@gmail.com>
2019-05-08 17:29:17 +0530

    Rendering data in the list component.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -12,16 +12,30 @@ const getBooksQuery = gql`
 `
 
 class BookList extends Component {
-  render() {
-        console.log(this.props);
+    displayBooks() {
+        var data = this.props.data;
+        if (data.loading) {
+            return(<div>Loading books...</div>);
+        } else {
+            return data.books.map(book => {
+                return(
+                    <li key={book.id}>
+                        {book.name}
+                    </li>
+                )
+            });
+        }
+    }
+
+    render() {
         return (
         <div>
             <ul id="book-list">
-                <li>Book name</li>
+                {this.displayBooks()}
             </ul>
         </div>
         )
-  }
+    }
 }
 
 export default graphql(getBooksQuery)(BookList);
